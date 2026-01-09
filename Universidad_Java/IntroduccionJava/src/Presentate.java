65eeeb0cd31f4f4db859994d7d8271fd7d4c9d6d
fcicerelli <fcicerellitic@gmail.com>
2026-01-09 20:27:29 -0300

    hola mundo y variables

---
+++ Universidad_Java/IntroduccionJava/src/Presentate.java
@@ -0,0 +1,7 @@
+public class Presentate {
+  public static void main(String[] args) {
+    System.out.println("Nombre Completo: Jacinto El Cartero");
+    System.out.println("Edad: 64 años");
+    System.out.println("País: Tangamandapio");
+  }
+}
---
+++ Universidad_Python/Variables/variables.py
@@ -0,0 +1,13 @@
+# Programa: Informacion personal
+
+# Guardar los datos en variables
+nombre = "Ana"
+edad = 22
+pais = 'México'
+frase_favorita = "Nunca dejes de aprender"
+
+# Mostramos la información en consola
+print("Nombre:", nombre)
+print("Edad:", edad)
+print("Pais:", pais)
+print("Frase_favorita:", frase_favorita)
